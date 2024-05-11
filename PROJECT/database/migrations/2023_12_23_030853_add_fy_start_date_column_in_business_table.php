dd4e9ec79c0eb93918db4252a39a74bd409e9371
Ahmii-Here <98882075+Ahmii-Here@users.noreply.github.com>
2024-05-12 02:13:21 +0500

    first commit

---
+++ PROJECT/database/migrations/2023_12_23_030853_add_fy_start_date_column_in_business_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('business', function (Blueprint $table) {
+            $table->string('fy_start_date')->default('01')->after('fy_start_month');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('business', function (Blueprint $table) {
+            $table->dropColumn('fy_start_date');
+        });
+    }
+};
